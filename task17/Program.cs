2b9deb4360adca4b39781d479dd8f5f2bd47d76d
sweetie <ivan.linzov@gmail.com>
2022-08-13 10:46:33 +0300

    realized task17

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты точки по X");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки по Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в первой координатной четверти");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Точка находится во второй координатной четверти");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в третьей координатной четверти");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в четвертой координатной четверти");
+}
+else
+{
+    Console.WriteLine("невозвожно определить координатную четверть точки");
+}
+
+
